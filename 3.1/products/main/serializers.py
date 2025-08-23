5ee8fda5a642a85e2886c44e03e818c5c10805b8
Daria Men <dariamen007@gmail.com>
2025-08-24 03:17:28 +0700

    Task finished

---
+++ 3.1/products/main/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from main.models import Product, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['text', 'mark']
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["title", "price"]
+
+
+class ProductDetailsSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ["title", "description", "price", "reviews"]
